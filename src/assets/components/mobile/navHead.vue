b1a73a5b33c7f0cb0ae735b69fb25514d1c29d2c
limanjia <853569768@qq.com>
2018-10-09 14:11:27 +0800

    定位修改

--- src/assets/components/mobile/navHead.vue
+++ src/assets/components/mobile/navHead.vue
@@ -62,7 +62,7 @@
     </div>
     </div>
 </template>
-
+<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=q94UM9A7kt6T105bvtoPWV5zzdiqWlGu"></script>
 <script>
 export default {
   data () {
